c6234605e3ad508812f5ae3ab04c599af97b34fa
Deniz-Can Sahin <sdenizcan7@gmail.com>
2023-05-12 10:21:46 +0000

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <div>
+      <h1 class="text-5xl">My first app</h1>
+    </div>
+  );
+};
+
+export default App;
