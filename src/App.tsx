23382716eda2c1f11d349b2e4785627402ce9305
Deniz-Can Sahin <sdenizcan7@gmail.com>
2023-05-12 10:28:53 +0000

    Add my name

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import type { Component } from "solid-js";
 
 const App: Component = () => {
+  const name = "Deniz";
+
   return (
     <div>
       <h1 class="text-5xl">My first app</h1>
+      <p>My name is {name}!</p>
     </div>
   );
 };
